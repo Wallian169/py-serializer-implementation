05848eeb19d3d630504b839b12de50d45b1c06dd
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-23 22:28:42 +0300

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -2,4 +2,16 @@ from rest_framework import serializers
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(min_value=0)
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(allow_null=True, required=False)
+
+    def validate(self, data):
+        if not data.get('is_broken') and data.get('problem_description'):
+            raise serializers.ValidationError(
+                "Cannot provide a problem description "
+                "if the car is not broken."
+            )
+        return data
